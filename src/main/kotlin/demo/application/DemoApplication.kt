a456f3e9fbaa2afeec35abb4f260c1bd699b0778
Maria Nepomyaschaya <marianepom555@gmail.com>
2024-12-21 18:12:18 +0300

    added @ComponentScan("repository")

--- src/main/kotlin/demo/application/DemoApplication.kt
+++ src/main/kotlin/demo/application/DemoApplication.kt
@@ -1,11 +1,15 @@
 package demo.application
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan("repository")
+
 class DemoApplication
 
 fun main(args: Array<String>) {
